371e16a32a3524f0d31e986c57a3cf79c3e26d72
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-08-26 15:46:12 -0300

    Mostrar mensagens do servidor implementado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 let participant;
 let idKeepConnection;
+let idUpdateMessages;
+
+const timeIntervalStatus = 5000;
+const timeIntervalUpdate = 3000;
 
 function processErrorLogon(reply) {
 	switch (reply.response.status) {
@@ -14,7 +18,7 @@ function processErrorLogon(reply) {
             alert("O recurso que você está tentando inserir já foi inserido");
             break;
         case (422):
-            alert("Formato insperado");
+            alert("Formato da mensagem insperado!");
             break;
         default:
             alert(`Erro ${reply.response.status}: ${reply.response.statusText}`)
@@ -25,6 +29,7 @@ function processErrorLogon(reply) {
 function processErrorStatus(reply) {
 	alert("Conexão perdida!")
     clearInterval(idKeepConnection);
+    clearInterval(idUpdateMessages);
 }
 
 function keepConnection() {
@@ -33,15 +38,14 @@ function keepConnection() {
 }
 
 function processResponseLogon(reply) {
-    idKeepConnection = setInterval(keepConnection, 5000);
+    idKeepConnection = setInterval(keepConnection, timeIntervalStatus);
+    idUpdateMessages = setInterval(updateMessages, timeIntervalUpdate);
 }
 
 function logon() {
     let nickname;
     do {
-        console.log(nickname);
         nickname = prompt("Insira seu nickname:");
-        console.log(nickname.trim().length);
         if (nickname === null || nickname === undefined || nickname.trim().length === 0) {
             alert("É necessário inserir um nickname!");
         }
@@ -49,7 +53,36 @@ function logon() {
     const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: nickname});
     promisse.then(processResponseLogon);
     promisse.catch(processErrorLogon);
-    return {name: nickname}
+    return {name: nickname};
+}
+
+function updateMessages() {
+    const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promisse.then(processResponseMessages);
+}   
+
+function canRead(message) {
+    if (message.to === "Todos" || message.to === participant.name) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function processResponseMessages(reply) {
+    const messages = reply.data.filter(canRead);
+    messages.forEach(showMessage);
+}
+
+function showMessage(message) {
+    const main = document.querySelector("main");
+    main.innerHTML +=  `<section class="message-box ${message.type}">
+                            <span class="message-content">
+                                <span class="time">(${message.time})</span>
+                                <span class="sender-receiver"><strong>${message.from}</strong> para <strong>${message.to}: </strong></span>
+                                <span class="text">${message.text}</span>
+                            </span>
+                        </section>`;
 }
 
 participant = logon();

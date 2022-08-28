1319400847e973a005f2022c612445bfbb56790a
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-08-27 21:43:09 -0300

    Atualização e envio de mensagens implementado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 let participant;
 let idKeepConnection;
 let idUpdateMessages;
+let messagesShown = [];
 
 const timeIntervalStatus = 5000;
 const timeIntervalUpdate = 3000;
 
-function processErrorLogon(reply) {
+function processError(reply) {
 	switch (reply.response.status) {
         case (400):
             alert("A conexão foi recusada!");
@@ -23,21 +24,17 @@ function processErrorLogon(reply) {
         default:
             alert(`Erro ${reply.response.status}: ${reply.response.statusText}`)
     }
-    logon();
-}
-
-function processErrorStatus(reply) {
-	alert("Conexão perdida!")
     clearInterval(idKeepConnection);
     clearInterval(idUpdateMessages);
+    logon();
 }
 
 function keepConnection() {
     const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", participant);
-    promisse.catch(processErrorStatus);
+    promisse.catch(processError);
 }
 
-function processResponseLogon(reply) {
+function processResponseLogon(response) {
     idKeepConnection = setInterval(keepConnection, timeIntervalStatus);
     idUpdateMessages = setInterval(updateMessages, timeIntervalUpdate);
 }
@@ -52,26 +49,40 @@ function logon() {
     } while (nickname === null || nickname === undefined || nickname.trim().length === 0);
     const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: nickname});
     promisse.then(processResponseLogon);
-    promisse.catch(processErrorLogon);
+    promisse.catch(processError);
     return {name: nickname};
 }
 
 function updateMessages() {
     const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promisse.then(processResponseMessages);
+    promisse.catch(processError);
 }   
 
 function canRead(message) {
     if (message.to === "Todos" || message.to === participant.name) {
         return true;
-    } else {
-        return false;
     }
 }
 
-function processResponseMessages(reply) {
-    const messages = reply.data.filter(canRead);
-    messages.forEach(showMessage);
+function newMessages(messagesArrived, lastMessageShown) {
+    let i;
+    for (i = messagesArrived.length - 1; i > 0; i--) {
+        if (Object.values(messagesArrived[i]) + "" === Object.values(lastMessageShown) + "") {
+            break;
+        }
+    }
+    return messagesArrived.slice(i + 1);
+}
+
+function processResponseMessages(response) {
+    let messagesArrived = response.data.filter(canRead);
+    if (messagesShown.length !== 0) {
+        const lastMessageShown = messagesShown[messagesShown.length - 1];
+        messagesArrived = newMessages(messagesArrived, lastMessageShown);
+    }
+    messagesShown = messagesShown.concat(messagesArrived);
+    messagesArrived.forEach(showMessage);
 }
 
 function showMessage(message) {
@@ -83,6 +94,24 @@ function showMessage(message) {
                                 <span class="text">${message.text}</span>
                             </span>
                         </section>`;
+    const messages = main.querySelectorAll(".message-box");
+    messages[messages.length - 1].scrollIntoView();
+}
+
+function sendMessage() {
+    const input = document.querySelector("footer input");
+    if (input.value.trim().length > 0) {
+        const messageToSend = {
+                                from: participant.name,
+                                to: "Todos",
+                                text: input.value.trim(),
+                                type: "message"
+                            };
+        const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", messageToSend);
+        promisse.then(updateMessages);
+        promisse.catch(processError);
+    }
+    input.value = "";
 }
 
 participant = logon();
